6c3660a3efbe17397d8532a17b91eb9df7b42089
Dan Neatu <dan.neatu@netcetera.com>
2023-06-17 21:48:45 +0200

    mod appearance

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html", "home.html"],
+  content: ["./src/**/*.{html,js}", "index.html", "home.html", "newstartpage.html"],
   theme: {
     extend: {},
   },
