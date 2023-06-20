a052e959f7c39ff955d9b9fd82d928ae6608d0a4
Dan Neatu <dan.neatu@netcetera.com>
2023-06-20 08:53:26 +0200

    add data transfer title

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "index.html", "home.html", "newstartpage.html"],
+  content: ["./src/**/*.{html,js}", "index.html", "home.html", "newstartpage.html", "datatransfer.html"],
   theme: {
     extend: {},
   },
